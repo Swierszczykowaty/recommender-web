2fdbc91e77fb9834dfc2a20ef8f681fcc50aa547
Michał Świercz <loolok1232@gmail.com>
2025-06-21 22:55:05 +0200

    [ADD] - Hero improvemnts

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import Title from '@/components/Title';
 import Container from '@/components/Container';
 
@@ -6,32 +7,34 @@ import React from 'react';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 
-
 export default function Hero() {
   return (
     <section className="relative min-h-screen flex items-center justify-center overflow-hidden px-4 md:px-8 py-16">
       <Container>
-        <div className='flex flex-col items-center justify-center relative z-10 w-full text-center'>
-          {/* Nagłówek sekcji */}
+        <div className="flex flex-col items-center justify-center relative z-10 w-full text-center">
           <motion.div
-          className="relative z-10 w-full max-w-4xl text-center my-20"
-          initial={{ opacity: 0, y:-20}} // Stan początkowy: niewidoczny i lekko poniżej
-          animate={{ opacity: 1, y: 0 }} // Stan końcowy: w pełni widoczny, na swojej pozycji
-          transition={{ duration: 0.6, ease: "easeOut" }} // Czas trwania, opóźnienie i funkcja przejścia
+            className="relative z-10 w-full max-w-4xl text-center my-20"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, ease: 'easeOut' }}
           >
-            <Title subtitle="Wybierz, co Cię interesuje">Rekomendacjie filmowe</Title>
+            <Title subtitle="Wybierz, co Cię interesuje">Rekomendacje filmowe</Title>
           </motion.div>
-          {/* Dwa linkowane boxy */}
-          <motion.div 
-          initial={{ opacity: 0, y: 50 }} // Stan początkowy: niewidoczny i lekko poniżej
-          animate={{ opacity: 1, y: 0 }} // Stan końcowy: w pełni widoczny, na swojej pozycji
-          transition={{ duration: 0.6, ease: "easeOut" }} // Czas trwania, opóźnienie i funkcja przejścia
+
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.6, ease: 'easeOut' }}
             className="relative z-10 flex flex-col md:flex-row items-stretch justify-center gap-6 md:gap-8 w-full max-w-6xl"
           >
             {/* Box 1 */}
-            <Link href="/recommender" className="group flex-1 min-h-[300px] flex flex-col justify-between bg-white/10 backdrop-blur-lg border border-white/20 rounded-xl px-6 py-12 text-center shadow-xl transition hover:bg-white/20">
+            <Link
+              href="/recommender"
+              className="group flex-1 min-h-[300px] flex flex-col justify-between bg-white/10 backdrop-blur-lg border border-white/20 rounded-xl px-6 py-12 text-center shadow-xl transition hover:bg-white/20"
+            >
               <div>
-                <h1 className="text-3xl font-bold text-white mb-4 ">Znajdź swój film</h1>
+                <span className="material-icons text-6xl md-60 text-white/80 mb-6">insights</span>
+                <h1 className="text-3xl font-bold text-white mb-4">Znajdź swój film</h1>
                 <p className="text-white/80 text-base mb-6">
                   Odkryj najlepsze rekomendacje filmowe dopasowane do Twoich upodobań. Kliknij i zobacz, co może Ci się spodobać.
                 </p>
@@ -43,9 +46,13 @@ export default function Hero() {
             </Link>
 
             {/* Box 2 */}
-            <Link href="/movies" className="group flex-1 min-h-[300px] flex flex-col justify-between bg-white/10 backdrop-blur-lg border border-white/20 rounded-xl px-6 py-12 text-center shadow-xl transition hover:bg-white/20">
+            <Link
+              href="/movies"
+              className="group flex-1 min-h-[300px] flex flex-col justify-between bg-white/10 backdrop-blur-lg border border-white/20 rounded-xl px-6 py-12 text-center shadow-xl transition hover:bg-white/20"
+            >
               <div>
-                <h1 className="text-3xl font-bold text-white mb-4 ">Sprawdź bazę filmów</h1>
+                <span className="material-icons text-6xl md-60 text-white/80 mb-6">storage</span>
+                <h1 className="text-3xl font-bold text-white mb-4">Sprawdź bazę filmów</h1>
                 <p className="text-white/80 text-base mb-6">
                   Przeglądaj pełną bazę filmów dostępnych w systemie, sprawdzaj ich opisy i wybierz coś dla siebie zanim uruchomisz rekomendacje.
                 </p>
--- src/components/SmallMovieCard.tsx
+++ src/components/SmallMovieCard.tsx
@@ -2,6 +2,7 @@
 
 import Image from 'next/image';
 import type { Movie } from '@/types/movie';
+import Link from 'next/link';
 
 interface SmallMovieCardProps {
   movie: Movie;
@@ -11,35 +12,47 @@ interface SmallMovieCardProps {
 export default function SmallMovieCard({ movie, onSelect }: SmallMovieCardProps) {
   return (
     <div
-      className="w-full flex items-center gap-6 p-3 bg-white/10 rounded-lg hover:bg-white/20 transition cursor-pointer"
-      onClick={() => onSelect(movie)}
+      className="w-full flex flex-col xl:flex-row items-center gap-6 p-3 bg-white/10 rounded-lg hover:bg-white/20 transition cursor-pointer"
     >
-      <div className="w-20 h-28 relative flex-shrink-0">
-        <Image
-          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          alt={movie.title}
-          fill
-          className="rounded object-cover"
-          sizes="96px"
-        />
-      </div>
+      <div className="flex items-start gap-4 w-full">
+        <div className="w-20 h-28 relative flex-shrink-0">
+          <Image
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.title}
+            fill
+            className="rounded object-cover"
+            sizes="96px"
+          />
+        </div>
 
-      <div className="flex-1 text-white overflow-hidden">
-        <h3 className="text-xl font-semibold truncate">{movie.title}</h3>
-        <p className="text-white/60 text-sm mt-1">{movie.release_date?.slice(0, 4)}</p>
-        <p className="text-white/70 text-sm">Ocena: {movie.vote_average?.toFixed(1) ?? '–'}</p>
-        <p className="text-white/80 text-sm truncate">
-          {Array.isArray(movie.genres)
-            ? movie.genres.join(', ')
-            : typeof movie.genres === 'string'
-            ? movie.genres
-            : 'Brak danych'}
-        </p>
+        <div className="flex-1 text-white overflow-hidden">
+          <h3 className="text-sm md:text-lg font-semibold line-clamp-2 ">{movie.title}</h3>
+          <p className="text-white/70 text-xs mt-1">{movie.release_date?.slice(0, 4)}</p>
+          <p className="text-white/70 text-xs line-clamp-1">Ocena: {movie.vote_average?.toFixed(1) ?? '–'}</p>
+          <p className="text-white/80 text-xs italic truncate">
+            {Array.isArray(movie.genres)
+              ? movie.genres.join(', ')
+              : typeof movie.genres === 'string'
+              ? movie.genres
+              : 'Brak danych'}
+          </p>
+        </div>
       </div>
-
-      <button className="ml-4 px-5 py-2 bg-white/20 hover:bg-white/30 text-white rounded cursor-pointer">
-        Wybierz
-      </button>
+      <div className='flex flex-row xl:flex-col items-end gap-2 w-full xl:w-auto '>
+        <Link
+          href={`/movies/${movie.id}`}
+          className="w-full ml-0 xl:ml-2 px-5 py-2 bg-white/20 hover:bg-white/30 text-white rounded cursor-pointer text-sm text-center"
+          >
+        <button>
+          Zobacz w bazie
+        </button>
+        </Link>
+        <button 
+        className="w-full ml-0 xl:ml-2 px-5 py-2 bg-amber-300/20 hover:bg-white/30 text-white rounded cursor-pointer text-sm "
+        onClick={() => onSelect(movie)}>
+          Generuj podobne
+        </button>
+      </div>
     </div>
   );
 }
