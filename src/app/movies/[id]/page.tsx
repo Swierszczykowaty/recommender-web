c713eb1280c496715a7435e2257a7828fa3e9072
Michał Świercz <loolok1232@gmail.com>
2025-07-02 17:16:55 +0200

    [ADD] - logo, modal file

--- src/app/movies/[id]/page.tsx
+++ src/app/movies/[id]/page.tsx
@@ -11,6 +11,7 @@ import ColorThief from "colorthief";
 import { useEffect } from "react";
 import AnimatedBackground from "@/components/global/Background";
 import { AnimatePresence } from "framer-motion";
+import MovieModal from "@/components/movies/MovieModal";
 
 const moviesData: Movie[] = rawMoviesData as Movie[];
 
@@ -228,47 +229,11 @@ export default function MovieDetailPage() {
 
       </Container>
 
-      <AnimatePresence>
-        {isModalOpen && (
-          <motion.div
-            className="fixed inset-0 z-50 flex items-center justify-center bg-black/80 backdrop-blur-md"
-            onClick={() => setIsModalOpen(false)}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.3 }}
-          >
-            <motion.div
-              className="relative w-[90%] mt-[68px]"
-              onClick={(e) => e.stopPropagation()}
-              initial={{ scale: 0.9, opacity: 0 }}
-              animate={{ scale: 1, opacity: 1 }}
-              exit={{ scale: 0.9, opacity: 0 }}
-              transition={{ duration: 0.3, ease: "easeOut" }}
-            >
-              <Image
-                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-                alt={movie.title}
-                width={1000}
-                height={1500}
-                className="rounded-lg object-contain max-h-[60vh] sm:max-h-[80vh] mx-auto"
-              />
-              <div className="text-center text-white mt-4">
-                <h2 className="text-xl font-bold">{movie.title}</h2>
-                <p className="text-sm text-white/70">
-                  {movie.release_date?.slice(0, 4)}
-                </p>
-              </div>
-              <button
-                onClick={() => setIsModalOpen(false)}
-                className="absolute top-[-50] sm:top-0 right-0 bg-white/10 border border-white/30 text-white px-4 py-2 rounded hover:bg-white/20 cursor-pointer"
-              >
-                ✕
-              </button>
-            </motion.div>
-          </motion.div>
-        )}
-      </AnimatePresence>
+    <MovieModal
+      isOpen={isModalOpen}
+      onClose={() => setIsModalOpen(false)}
+      movie={movie}
+    />
     </section>
   );
 }
