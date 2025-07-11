ba77238e280bb3500d0786b8d59dbc7713cd9d30
Michał Świercz <loolok1232@gmail.com>
2025-07-11 17:58:21 +0200

    [FIX] - Movies files

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,5 +1,23 @@
-import MoviesPageWrapper from './MoviesPageWrapper';
+// src/app/movies/page.tsx
+import { Suspense }      from "react";
+import dynamic            from "next/dynamic";
+import moviesDataRaw      from "@/data/full_data_web.json";
+import type { Movie }     from "@/types/movie";
+
+const MoviesList = dynamic(() => import("@/components/movies/MovieList"));
 
 export default function Page() {
-  return <MoviesPageWrapper />;
+  const movies = moviesDataRaw as Movie[];
+
+  return (
+    <Suspense
+      fallback={
+        <div className="text-white text-center py-10">
+          Ładowanie filmów...
+        </div>
+      }
+    >
+      <MoviesList movies={movies} />
+    </Suspense>
+  );
 }
