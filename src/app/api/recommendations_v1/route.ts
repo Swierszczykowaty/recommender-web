302e8d5f20da40136e10eb12b7fb73f398812661
Michał Świercz <loolok1232@gmail.com>
2025-10-20 15:40:15 +0200

    [FIX] local api

--- src/app/api/recommendations_v1/route.ts
+++ src/app/api/recommendations_v1/route.ts
@@ -1,7 +1,7 @@
 export async function POST(req: Request) {
   const body = await req.text();
-  const upstream = "http://localhost:8000/api/v1/recommendations";
-  //const upstream = "https://recommender-api-f6qb.onrender.com/api/v1/recommendations";
+  //const upstream = "http://localhost:8000/api/v1/recommendations";
+  const upstream = "https://recommender-api-f6qb.onrender.com/api/v1/recommendations";
   const r = await fetch(upstream, { method: "POST", headers: { "content-type": "application/json" }, body, cache: "no-store" });
   const txt = await r.text();
   return new Response(txt, { status: r.status, headers: { "content-type": r.headers.get("content-type") ?? "application/json" } });
